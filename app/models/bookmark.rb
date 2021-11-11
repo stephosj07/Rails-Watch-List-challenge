779a29e73fcf8b4bacc89acef9af7bc24f1938be
Stephanie Julien <stephanie.julien7@gmail.com>
2021-11-11 12:35:01 -0500

    add missing validation to have movie and list pairings to be unique

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,6 +5,7 @@ class Bookmark < ApplicationRecord
   validates :movie_id, presence: true
   validates :list_id, presence: true
   validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 
 end
 
